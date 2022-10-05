4aaa598ee5230959d5c02a3d97c500e4628d006b
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-10-05 18:21:13 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
 
 const form = document.querySelector("form");
-const input = document.querySelectorAll("input");
+const input = document.querySelector("input");
 const display = document.getElementById("display");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const searchInput = input[0].value;
-  console.log(searchInput);
-  receiveApy();
+  const valueInput = input.value;
+  console.log(valueInput);
+  receiveApy(valueInput);
 });
 
-function receiveApy() {
+function receiveApy(searchInput) {
   fetch(
-    "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}"
+    `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
   )
     .then((res) => res.json())
     .then((res) => {
-      const display = res.query.search;
-      console.log(display);
+      const data = res.query.search;
+      console.log(data);
+      displayResult(data);
     });
 }
 
-receiveApy();
-
-/* display.textContent = data.text_head;*/
+function displayResult(data) {
+  data.forEach((ns) => {
+    const url = `https://en.wikipedia.org/?curid=${ns.pageid}`;
+    const card = document.createElement("div");
+    card.className = "result-item";
+    card.innerHTML = `
+    <h3><a href=${url}>${ns.title}</a></h3>
+    <a href=${url} class="result-link">${url}</a>
+    <span class="result-snippet">${ns.snippet}</span>
+    `;
+    display.appendChild(card);
+  });
+}
