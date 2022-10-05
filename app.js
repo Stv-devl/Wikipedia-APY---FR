d165f37ed766fe98507537413712c3c68bcc5a47
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-10-05 15:46:00 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,15 +2,26 @@
 
 const form = document.querySelector("form");
 const input = document.querySelectorAll("input");
+const display = document.getElementById("display");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const searchInput = input[0].value;
   console.log(searchInput);
+  receiveApy();
 });
 
-fetch(
-  "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}"
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+function receiveApy() {
+  fetch(
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}"
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      const display = res.query.search;
+      console.log(display);
+    });
+}
+
+receiveApy();
+
+/* display.textContent = data.text_head;*/
