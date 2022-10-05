e4bc7649c7c65879dcb1025aac7ebdc003107f39
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-10-05 14:36:25 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
 
-// fetch("url", "options").then(
-//   (response) => {
-//     // response
-//   }).catch((err) => console.log(err))
-// );
+const form = document.querySelector("form");
+const input = document.querySelectorAll("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchInput = input[0].value;
+  console.log(searchInput);
+});
+
+fetch(
+  "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}"
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
